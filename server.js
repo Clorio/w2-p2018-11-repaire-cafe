c9fd0c491e2c551df7a4099f3954f17e5a635e7d
renanbronchart <renan.bronchart@gmail.com>
2017-01-13 00:59:46 +0100

    [Frontend] Fix webSite architecture and add first route of home

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const expressLayouts = require('express-ejs-layouts');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(expressLayouts);
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(require('./app/routes'));
+
+app.listen(port, () => {
+  console.log(`server is listening on port: ${port}`);
+});
